d99835f219748c4e26538ed684ce20fd196d2722
DRinsvind <adskiyprikol@gmail.com>
2018-07-08 14:17:46 +0300

    add middlewares

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Joi = require('joi');
+const logger = require('./logger')
 const express = require('express');
 const app = express();
+const helmet = require('helmet')
+const morgan = require('morgan')
 
-app.use(express.json());
+app.use(express.json()); // req.body
+app.use(express.urlencoded({ extended: true }));//for form/urlencoded key=value&key=value
+app.use(express.static('public'));
+app.use(helmet())
+app.use(morgan('tiny'))
+
+app.use(logger);
 
 var courses = [
     { id: 1, name: 'course1'},
