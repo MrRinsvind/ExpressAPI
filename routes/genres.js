c0f3df83b2793b8feb9590baeea99ccba010dbd1
DRinsvind <adskiyprikol@gmail.com>
2018-07-19 22:58:42 +0300

    add error and express-async-errors middlewares

--- routes/genres.js
+++ routes/genres.js
@@ -1,8 +1,12 @@
+const asyncMiddleware = require('../middleware/async')
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
 const { Genre, validate} = require('../models/genre')
 const express = require('express')
 const router = express.Router();
+
+
+
 
 router.get('/', async(req, res) => {
   const genres = await Genre.find().sort('name');
